7bcb0a12b863bcaffe203d4652efbd6af2e9f463
David Koudela <dkoudela@volny.cz>
2016-10-01 13:31:07 -0700

    Experiments with standalone web service.

--- src/main/java/com/davidkoudela/confluence/controlledpm/cloud/connect/ControlledPMResourceManager.java
+++ src/main/java/com/davidkoudela/confluence/controlledpm/cloud/connect/ControlledPMResourceManager.java
@@ -21,22 +21,21 @@ public class ControlledPMResourceManager {
         final String[] filters = { "bptemplates", "images" };
         List<String> resources = new LinkedList<String>();
         if (jarFile.isFile()) { // Executed from JRE directly
-            System.out.println("It is a file");
             final JarFile jar = new JarFile(jarFile);
             final Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
             while(entries.hasMoreElements()) {
                 final String name = entries.nextElement().getName();
-                //if (name.startsWith(path + "/")) { //filter according to the path
-                System.out.println(name);
-                //}
+                for (String filter : filters) {
+                    if (name.startsWith(filter)) {
+                        System.out.println(name);
+                    }
+                }
             }
             jar.close();
         } else { // Executed from IDE
-            System.out.println("Not a file");
             for (File file : jarFile.listFiles()) {
                 for (String filter : filters) {
                     if (0 == file.getName().compareTo(filter)) {
-                        System.out.println("It is a dir: " + file.getName());
                         for (File file2 : file.listFiles()) {
                             System.out.println("Content : " + file2.getAbsolutePath());
                             resources.add('/' + file.getName() + '/' + file2.getName());
